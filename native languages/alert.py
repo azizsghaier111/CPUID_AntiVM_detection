ba8686efecb7ad77ec378c9ca44ca96ce12594b5
Mohamed Aziz Sghaier <98357635+azizsghaier111@users.noreply.github.com>
2024-11-12 11:21:56 +0100

    Update alert.py

---
+++ native languages/alert.py
@@ -0,0 +1,17 @@
+from ghidra.program.model.listing import Listing
+from ghidra.program.model.listing import Instruction
+from ghidra.util.task import ConsoleTaskMonitor
+
+def detect_cpuid_instructions():
+    print("here I started the detection")
+    listing = currentProgram.getListing()
+    monitor = ConsoleTaskMonitor()
+    
+    # Iterate over all instructions in the current program
+    for instruction in listing.getInstructions(True):
+        if instruction.getMnemonicString() == "CPUID":
+            addr = instruction.getAddress()
+            print("CPUID instruction found at address: {}".format(addr))
+            popup("CPUID instruction detected at address: {}".format(addr))
+
+detect_cpuid_instructions()
